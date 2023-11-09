f41bd0a806e0026b1d2f5cc74196f49665f2a7d1
Sselimkoc <56224042+Sselimkoc@users.noreply.github.com>
2023-11-09 14:29:23 +0300

    Update firebase-cf.js

--- src/config/firebase-cf.js
+++ src/config/firebase-cf.js
@@ -4,13 +4,7 @@ import { getDatabase } from "firebase/database";
 import { getFirestore, collection, addDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCY4T14lYZnjgO_OHhx1AtJw7QgekIk370",
-  authDomain: "no-tomorrow-b1a82.firebaseapp.com",
-  projectId: "no-tomorrow-b1a82",
-  databaseURL: "https://no-tomorrow-b1a82-default-rtdb.firebaseio.com/",
-  storageBucket: "no-tomorrow-b1a82.appspot.com",
-  messagingSenderId: "974862683742",
-  appId: "1:974862683742:web:8ea9738e3d60448e142ecf",
+ 
 };
 
 const app = initializeApp(firebaseConfig);
